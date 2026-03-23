fbca1d736ba75127d36e0dc66389e5cf6241b1b5
palaminiauto <andrewpala07@gmail.com>
2026-03-23 19:22:44 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-const SPREADSHEET_ID = 'IL_TUO_SPREADSHEET_ID'; // stesso di prima
+const SPREADSHEET_ID = '1_Kkvn4ziKQE_qKakv_pH_pnfkWTMvTB618S1f0onA2g'; // stesso di prima
 const WORKER_URL = 'https://palaminiapp.andrewpala07.workers.dev/api';
 const APP_SECRET = 'palamini2026';
 const FOGLI_DA_ESCLUDERE = ['IMPOSTAZIONI APP','PROGRAMMAZIONE','ORARI','AUTO'];
